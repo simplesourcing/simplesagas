703e12bc7e80fb8c866f9c2497a70b708b093084
szoio <stephenzoio@yahoo.com>
2018-12-03 14:17:53 +1100

    finished AsyncApp action processor

--- modules/action/src/main/java/io/simplesource/saga/action/async/AsyncApp.java
+++ modules/action/src/main/java/io/simplesource/saga/action/async/AsyncApp.java
@@ -20,191 +20,92 @@ import io.simplesource.saga.shared.utils.StreamAppUtils;
 import lombok.Value;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class AsyncApp<A> {
 
-    Logger logger = LoggerFactory.getLogger(AsyncApp.class);
-    List<String> expectedTopicList = new ArrayList<>();
-    List<AsyncTransformer> transformers = new ArrayList<>();
-    List<Supplier<Integer>> closeHandlers = new ArrayList<>();
+    private final Logger logger = LoggerFactory.getLogger(AsyncApp.class);
+    private final List<String> expectedTopicList = new ArrayList<>();
+    private final List<AsyncTransformer<A>> transformers = new ArrayList<>();
+    private final List<Supplier<Integer>> closeHandlers = new ArrayList<>();
 
     private final List<TopicCreation> expectedTopics;
     private final TopicConfig actionTopicConfig;
     private final ActionProcessorSpec<A> actionSpec;
 
-
     @Value
-    static class AsyncTransformerInput<A> {
+    private static class AsyncTransformerInput<A> {
         final StreamsBuilder builder;
         final KStream<UUID, ActionRequest<A>> actionRequests;
         final KStream<UUID, ActionResponse> actionResponses;
     }
 
-    interface AsyncTransformer<A> {
+    private interface AsyncTransformer<A> {
         Function<Properties, AsyncTransform.AsyncPipe> apply(AsyncTransformerInput<A> input);
     }
 
-    AsyncApp(ActionSerdes<A> actionSerdes, TopicConfigBuilder.BuildSteps topicBuildFn) {
+    public AsyncApp(ActionSerdes<A> actionSerdes, TopicConfigBuilder.BuildSteps topicBuildFn) {
         expectedTopicList.addAll(TopicTypes.ActionTopic.all);
         expectedTopicList.add(TopicTypes.ActionTopic.requestUnprocessed);
 
         actionTopicConfig = TopicConfigBuilder.buildTopics(expectedTopicList, new HashMap<>(), new HashMap<>(), topicBuildFn);
         actionSpec = new ActionProcessorSpec<A>(actionSerdes);
-        expectedTopics = TopicCreation.allTopics(actionTopicConfig)
+        expectedTopics = TopicCreation.allTopics(actionTopicConfig);
 
     }
 
     <I, K, O, R> AsyncApp<A> addAsync(AsyncSpec<A, I, K, O, R> spec) {
         AsyncContext<A, I, K, O, R> ctx = new AsyncContext<>(actionSpec, actionTopicConfig.namer, spec);
         AsyncTransformer<A> transformer = input -> {
-
             // join the action request with corresponding prior command responses
             AsyncStream.addSubTopology(ctx, input.actionRequests, input.actionResponses);
 
             return AsyncTransform.async(ctx);
         };
         transformers.add(transformer);
-        spec.outputSpec.ifPresent(oSpec -> {
-            expectedTopics.addAll(oSpec.getTopicCreations());
-        });
-
+        spec.outputSpec.ifPresent(oSpec -> expectedTopics.addAll(oSpec.getTopicCreations()));
         return this;
     }
 
-  void addCloseHandler(Supplier<Integer> handler) {
-    closeHandlers.add(handler);
-  }
+    public void addCloseHandler(Supplier<Integer> handler) {
+        closeHandlers.add(handler);
+    }
 
+    public void run(StreamAppConfig appConfig) {
+        Properties config = StreamAppConfig.getConfig(appConfig);
 
-  void run(StreamAppConfig appConfig) {
-
-      Properties  config = StreamAppConfig.getConfig(appConfig);
-
-    StreamAppUtils
-      .addMissingTopics(AdminClient.create(config), expectedTopics)
-      .all()
-      .get(30L, TimeUnit.SECONDS);
-
-      StreamsBuilder builder = new StreamsBuilder();
-    KStream<UUID, ActionRequest<A>> actionRequests =
-      ActionConsumer.actionRequestStream(actionSpec, actionTopicConfig.namer, builder);
-      KStream<UUID, ActionResponse>  actionResponses =
-      ActionConsumer.actionResponseStream(actionSpec, actionTopicConfig.namer, builder);
-
-      AsyncTransformerInput<A> commandInput          = new AsyncTransformerInput<>(builder, actionRequests, actionResponses);
-      List<AsyncTransform.AsyncPipe> pipes  = Collectors.toList(transformers.stream().map(x -> x.apply(commandInput).apply(config));
-
-    val topology = builder.build()
-    logger.info("Topology description {}", topology.describe())
-    StreamAppUtils.runStreamApp(config, topology)
-
-    Runtime.getRuntime.addShutdownHook(new Thread {
-      override def run(): Unit = {
-        logger.info("Shutting down AsyncTransformers")
-        pipes.foreach(_.close())
-
-        closeHandlers.foreach { handler =>
-          handler.apply()
+        try {
+            StreamAppUtils
+                    .addMissingTopics(AdminClient.create(config), expectedTopics)
+                    .all()
+                    .get(30L, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to create missing topics");
         }
-      }
-    })
-  }
 
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<UUID, ActionRequest<A>> actionRequests =
+                ActionConsumer.actionRequestStream(actionSpec, actionTopicConfig.namer, builder);
+        KStream<UUID, ActionResponse> actionResponses =
+                ActionConsumer.actionResponseStream(actionSpec, actionTopicConfig.namer, builder);
+
+        AsyncTransformerInput<A> commandInput = new AsyncTransformerInput<>(builder, actionRequests, actionResponses);
+        List<AsyncTransform.AsyncPipe> pipes = transformers.stream().map(x -> {
+            Function<Properties, AsyncTransform.AsyncPipe> propsToPipe = x.apply(commandInput);
+            return propsToPipe.apply(config);
+        }).collect(Collectors.toList());
+
+        Topology topology = builder.build();
+        logger.info("Topology description {}", topology.describe());
+        StreamAppUtils.runStreamApp(config, topology);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutting down AsyncTransformers");
+            pipes.forEach(AsyncTransform.AsyncPipe::close);
+            closeHandlers.forEach(Supplier::get);
+        }));
+    }
 }
-
-
-//  private final TopicConfig actionTopicConfig;
-//  private final ActionProcessorSpec<A> actionSpec;
-//  private final Logger logger;
-//
-//  private final List<Command> commands = new ArrayList<>();
-//  private final List<TopicCreation> topicCreations;
-//
-//  @Value
-//  static final class CommandInput<A> {
-//    final StreamsBuilder builder;
-//    final KStream<UUID, ActionRequest<A>> actionRequests;
-//    final KStream<UUID, ActionResponse> actionResponses;
-//  }
-//
-//  static interface Command<A> {
-//    void applyCommandInput(CommandInput<A> input);
-//  }
-//
-//
-
-//final case class AsyncApp<A>(actionSerdes: ActionSerdes<A>, topicBuildFn: TopicConfigBuilder.BuildSteps) {
-//  private val logger = LoggerFactory.getLogger(classOf<AsyncApp<A>>)
-//
-//  final case class AsyncTransformerInput(builder: StreamsBuilder,
-//                                         actionRequests: KStream<UUID, ActionRequest<A>>,
-//                                         actionResponses: KStream<UUID, ActionResponse>)
-//
-//  val expectedTopicList = TopicTypes.ActionTopic.requestUnprocessed :: TopicTypes.ActionTopic.all
-//
-//  private val actionTopicConfig = TopicConfigBuilder.buildTopics(expectedTopicList, Map.empty)(topicBuildFn)
-//  private val actionSpec        = ActionProcessorSpec<A>(actionSerdes)
-//
-//  type AsyncTransformer = AsyncTransformerInput => Properties => AsyncPipe
-//
-//  private var transformers: List<AsyncTransformer> = List.empty
-//  private var expectedTopics                       = TopicCreation.allTopics(actionTopicConfig)
-//
-//  private var closeHandlers: List<() => Unit> = List.empty
-//
-//  def addAsync<I, K, O, R>(spec: AsyncSpec<A, I, K, O, R>)(
-//      implicit executionContext: ExecutionContext): AsyncApp<A> = {
-//    val ctx = AsyncContext(actionSpec, actionTopicConfig.namer, spec)
-//    val transformer: AsyncTransformer = input => {
-//
-//      // join the action request with corresponding prior command responses
-//      AsyncStream.addSubTopology<A, I, K, O, R>(ctx, input.actionRequests, input.actionResponses)
-//
-//      new AsyncPipe { override def close(): Unit = {} }
-//      AsyncTransform.async(ctx)
-//    }
-//    transformers = transformer :: transformers
-//    expectedTopics = expectedTopics ++ spec.outputSpec.fold(List.empty<TopicCreation>)(_.topicCreation)
-//    this
-//  }
-//
-//  def addCloseHandler(handler: => Unit): Unit = {
-//    closeHandlers = (() => handler) :: closeHandlers
-//  }
-//
-//  def run(appConfig: StreamAppConfig): Unit = {
-//    val config = StreamAppUtils.getConfig(appConfig)
-//
-//    StreamAppUtils
-//      .addMissingTopics(AdminClient.create(config))(expectedTopics.distinct)
-//      .all()
-//      .get(30L, TimeUnit.SECONDS)
-//
-//    val builder = new StreamsBuilder()
-//    val actionRequests: KStream<UUID, messages.ActionRequest<A>> =
-//      ActionConsumer.actionRequestStream(actionSpec, actionTopicConfig.namer, builder)
-//    val actionResponses: KStream<UUID, messages.ActionResponse> =
-//      ActionConsumer.actionResponseStream(actionSpec, actionTopicConfig.namer, builder)
-//
-//    val commandInput          = AsyncTransformerInput(builder, actionRequests, actionResponses)
-//    val pipes: Seq<AsyncPipe> = transformers.map(x => x(commandInput)(config))
-//
-//    val topology = builder.build()
-//    logger.info("Topology description {}", topology.describe())
-//    StreamAppUtils.runStreamApp(config, topology)
-//
-//    Runtime.getRuntime.addShutdownHook(new Thread {
-//      override def run(): Unit = {
-//        logger.info("Shutting down AsyncTransformers")
-//        pipes.foreach(_.close())
-//
-//        closeHandlers.foreach { handler =>
-//          handler.apply()
-//        }
-//      }
-//    })
-//  }
-//}
--- modules/action/src/main/java/io/simplesource/saga/action/async/AsyncTransform.java
+++ modules/action/src/main/java/io/simplesource/saga/action/async/AsyncTransform.java
@@ -8,8 +8,6 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serde;
 
-import scala.concurrent.ExecutionContext;
-
 final public class AsyncTransform {
     static final boolean useTransactions = false;
 
@@ -29,7 +27,7 @@ final public class AsyncTransform {
     }
 
 
-    static <A, I, K, O, R, runner> Function<Properties, AsyncPipe> async(AsyncContext<A, I, K, O, R> asyncContext) {
+    static <A, I, K, O, R> Function<Properties, AsyncPipe> async(AsyncContext<A, I, K, O, R> asyncContext) {
         return
                 config -> {
                     AsyncSpec<A, I, K, O, R> asyncSpec = asyncContext.asyncSpec;
@@ -61,56 +59,4 @@ final public class AsyncTransform {
                 };
     }
 
-};
-//
-//
-//object AsyncTransform {
-//  val useTransactions: Boolean = false
-//
-//  trait AsyncPipe {
-//    def close()
-//  }
-//
-//  implicit class ProdRecordOps<K, V>(record: ProducerRecord<K, V>) {
-//    def toByteArray(kSerde: Serde<K>, vSerde: Serde<V>): ProducerRecord<Array<Byte>, Array<Byte>> = {
-//      val topicName = record.topic()
-//      val kb        = kSerde.serializer().serialize(topicName, record.key())
-//      val kv        = vSerde.serializer().serialize(topicName, record.value())
-//      new ProducerRecord<Array<Byte>, Array<Byte>>(topicName, kb, kv)
-//    }
-//  }
-//
-//  def async<A, I, K, O, R>(asyncContext: AsyncContext<A, I, K, O, R>)(
-//      implicit executionContext: ExecutionContext): Properties => AsyncPipe = config => {
-//    val asyncSpec = asyncContext.asyncSpec
-//
-//    def copyProperties(properties: Properties) = {
-//      val newProps = new Properties()
-//      properties.forEach((key, value) => {
-//        newProps.setProperty(key.toString, value.toString)
-//        ()
-//      })
-//      newProps
-//    }
-//
-//    val consumerConfig: Properties = copyProperties(config)
-//    //consumerConfig.putAll(spec.config)
-//    consumerConfig.setProperty(ConsumerConfig.GROUP_ID_CONFIG,
-//                               asyncSpec.groupId + "_async_consumer_" + asyncSpec.actionType)
-//    // For now automatic - probably rather do this manually
-//    consumerConfig.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
-//    consumerConfig.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
-//    consumerConfig.setProperty(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed")
-//
-//    val producerProps = copyProperties(config)
-//    if (useTransactions)
-//      producerProps.setProperty(ProducerConfig.TRANSACTIONAL_ID_CONFIG, asyncSpec.groupId + "_async_producer")
-//    producerProps.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true")
-//
-//    new AsyncPipe {
-//      val runner = new ConsumerRunner(asyncContext, consumerConfig, producerProps)
-//      new Thread(runner).start()
-//      override def close(): Unit = runner.close()
-//    }
-//  }
-//}
+}
