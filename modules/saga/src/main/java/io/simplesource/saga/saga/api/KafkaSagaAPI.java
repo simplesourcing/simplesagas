934bbafc5a67d3aedc63b458d71d463439f42550
szoio <stephenzoio@yahoo.com>
2018-12-07 23:04:33 +1100

    wip

--- modules/saga/src/main/java/io/simplesource/saga/saga/api/KafkaSagaAPI.java
+++ modules/saga/src/main/java/io/simplesource/saga/saga/api/KafkaSagaAPI.java
@@ -1,4 +1,101 @@
-//package io.simplesource.saga.saga.api
+package io.simplesource.saga.saga.api;
+
+import io.simplesource.data.FutureResult;
+import io.simplesource.data.NonEmptyList;
+import io.simplesource.data.Result;
+import io.simplesource.kafka.dsl.KafkaConfig;
+import io.simplesource.kafka.internal.client.RequestAPIContext;
+import io.simplesource.kafka.spec.WindowSpec;
+import io.simplesource.saga.model.api.SagaAPI;
+import io.simplesource.saga.model.messages.SagaRequest;
+import io.simplesource.saga.model.messages.SagaResponse;
+import io.simplesource.saga.model.saga.SagaError;
+import io.simplesource.saga.model.serdes.SagaSerdes;
+import io.simplesource.saga.model.specs.SagaSpec;
+import io.simplesource.saga.shared.topics.TopicConfig;
+import io.simplesource.saga.shared.topics.TopicTypes;
+
+import java.time.Duration;
+import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public final class KafkaSagaAPI<A> implements SagaAPI<A> {
+    public KafkaSagaAPI(SagaSpec<A> sagaSpec,
+                 KafkaConfig kConfig,
+                 TopicConfig sagaTopicConfig,
+                 String clientId,
+                 ScheduledExecutorService scheduler) {
+        SagaSerdes<A> serdes = sagaSpec.serdes;
+
+        RequestAPIContext<UUID, SagaRequest<A>, SagaResponse> apiContext = RequestAPIContext
+                .<UUID, SagaRequest<A>, SagaResponse>builder()
+                .kafkaConfig(kConfig)
+                .requestTopic(sagaTopicConfig.namer.apply(TopicTypes.SagaTopic.request))
+                .responseTopicMapTopic(sagaTopicConfig.namer.apply(TopicTypes.SagaTopic.responseTopicMap))
+                .privateResponseTopic(sagaTopicConfig.namer.apply(TopicTypes.SagaTopic.response) + "_" + clientId)
+                .requestKeySerde(serdes.uuid())
+                .requestValueSerde(serdes.request())
+                .responseKeySerde(serdes.uuid())
+                .responseValueSerde(serdes.response())
+                .responseWindowSpec(new WindowSpec(TimeUnit.DAYS.toSeconds(7)))
+                .outputTopicConfig(sagaTopicConfig.topicSpecs.get(TopicTypes.SagaTopic.response))
+                .errorValue((request, error) -> new SagaResponse(request.sagaId, Result.failure(new SagaError(NonEmptyList.of(error.getMessage())))))
+    .scheduler(scheduler)
+                .build();
+    }
+
+    @Override
+    public FutureResult<SagaError, UUID> submitSaga(SagaRequest<A> request) {
+        return null;
+    }
+
+    @Override
+    public FutureResult<SagaError, SagaResponse> getSagaResponse(UUID requestId, Duration timeout) {
+        return null;
+    }
+}
+
+//
+//class KafkaSagaAPI<A>(sagaSpec: SagaSpec<A>,
+//                      kConfig: KafkaConfig,
+//                      sagaTopicConfig: TopicConfig,
+//                      clientId: String,
+//                      scheduler: ScheduledExecutorService)
+//    extends SagaAPI<A> {
+//  import FutureResultOps._
+//
+//  private val apiContext = RequestAPIContext
+//    .builder<UUID, SagaRequest<A>, SagaResponse>
+//    .kafkaConfig(kConfig)
+//    .requestTopic(sagaTopicConfig.namer(TopicTypes.SagaTopic.request))
+//    .responseTopicMapTopic(sagaTopicConfig.namer(TopicTypes.SagaTopic.responseTopicMap))
+//    .privateResponseTopic(sagaTopicConfig.namer(TopicTypes.SagaTopic.response) + "_" + clientId)
+//    .requestKeySerde(sagaSpec.serdes.uuid)
+//    .requestValueSerde(sagaSpec.serdes.request)
+//    .responseKeySerde(sagaSpec.serdes.uuid)
+//    .responseValueSerde(sagaSpec.serdes.response)
+//    .responseWindowSpec(new WindowSpec(TimeUnit.DAYS.toSeconds(7)))
+//    .outputTopicConfig(sagaTopicConfig.topicSpecs(TopicTypes.SagaTopic.response))
+//    .errorValue((request, error) => SagaResponse(request.sagaId, Left(SagaError.of(error.getMessage))))
+//    .scheduler(scheduler)
+//    .build()
+//
+//  private val requestAPI = new KafkaRequestAPI<UUID, SagaRequest<A>, SagaResponse>(apiContext)
+//
+//  override def submitSaga(request: SagaRequest<A>): Future<UUID> = {
+//    val result = requestAPI.publishRequest(request.sagaId, request.sagaId, request)
+//    result.asScalaFuture.map(_ => request.sagaId)
+//  }
+//
+//  override def getSagaResponse(requestId: UUID, timeout: Duration): Future<SagaResponse> = {
+//    val result: CompletableFuture<SagaResponse> =
+//      requestAPI.queryResponse(requestId, java.time.Duration.ofMillis(timeout.toMillis))
+//
+//    result.asScalaFuture
+//  }
+//}
+
 //
 //import java.util.UUID
 //import java.util.concurrent.{CompletableFuture, ScheduledExecutorService, TimeUnit}
@@ -19,10 +116,10 @@
 //import scala.concurrent.{Future, Promise}
 //
 //object FutureResultOps {
-//  implicit class FPOps[A](fr: FutureResult[Exception, A]) {
-//    def asScalaFuture: Future[A] = {
-//      val p = Promise[A]
-//      fr.fold[Unit]((e: NonEmptyList[Exception]) => {
+//  implicit class FPOps<A>(fr: FutureResult<Exception, A>) {
+//    def asScalaFuture: Future<A> = {
+//      val p = Promise<A>
+//      fr.fold<Unit>((e: NonEmptyList<Exception>) => {
 //        p.failure(e.head())
 //      }, (a: A) => {
 //        p.success(a)
@@ -31,10 +128,10 @@
 //    }
 //  }
 //
-//  implicit class JFOps[A](jf: CompletableFuture[A]) {
-//    def asScalaFuture: Future[A] = {
-//      val p = Promise[A]
-//      def handlerFn: BiFunction[A, Throwable, Int] = (a, e) => {
+//  implicit class JFOps<A>(jf: CompletableFuture<A>) {
+//    def asScalaFuture: Future<A> = {
+//      val p = Promise<A>
+//      def handlerFn: BiFunction<A, Throwable, Int> = (a, e) => {
 //        if (e != null) {
 //          p.failure(e)
 //        }
@@ -43,22 +140,22 @@
 //        }
 //        0
 //      }
-//      jf.handleAsync[Int](handlerFn)
+//      jf.handleAsync<Int>(handlerFn)
 //      p.future
 //    }
 //  }
 //}
 //
-//class KafkaSagaAPI[A](sagaSpec: SagaSpec[A],
+//class KafkaSagaAPI<A>(sagaSpec: SagaSpec<A>,
 //                      kConfig: KafkaConfig,
 //                      sagaTopicConfig: TopicConfig,
 //                      clientId: String,
 //                      scheduler: ScheduledExecutorService)
-//    extends SagaAPI[A] {
+//    extends SagaAPI<A> {
 //  import FutureResultOps._
 //
 //  private val apiContext = RequestAPIContext
-//    .builder[UUID, SagaRequest[A], SagaResponse]
+//    .builder<UUID, SagaRequest<A>, SagaResponse>
 //    .kafkaConfig(kConfig)
 //    .requestTopic(sagaTopicConfig.namer(TopicTypes.SagaTopic.request))
 //    .responseTopicMapTopic(sagaTopicConfig.namer(TopicTypes.SagaTopic.responseTopicMap))
@@ -73,15 +170,15 @@
 //    .scheduler(scheduler)
 //    .build()
 //
-//  private val requestAPI = new KafkaRequestAPI[UUID, SagaRequest[A], SagaResponse](apiContext)
+//  private val requestAPI = new KafkaRequestAPI<UUID, SagaRequest<A>, SagaResponse>(apiContext)
 //
-//  override def submitSaga(request: SagaRequest[A]): Future[UUID] = {
+//  override def submitSaga(request: SagaRequest<A>): Future<UUID> = {
 //    val result = requestAPI.publishRequest(request.sagaId, request.sagaId, request)
 //    result.asScalaFuture.map(_ => request.sagaId)
 //  }
 //
-//  override def getSagaResponse(requestId: UUID, timeout: Duration): Future[SagaResponse] = {
-//    val result: CompletableFuture[SagaResponse] =
+//  override def getSagaResponse(requestId: UUID, timeout: Duration): Future<SagaResponse> = {
+//    val result: CompletableFuture<SagaResponse> =
 //      requestAPI.queryResponse(requestId, java.time.Duration.ofMillis(timeout.toMillis))
 //
 //    result.asScalaFuture
--- modules/saga/src/main/java/io/simplesource/saga/saga/builder/SagaClientBuilder.java
+++ modules/saga/src/main/java/io/simplesource/saga/saga/builder/SagaClientBuilder.java
@@ -1,33 +1,89 @@
-//package io.simplesource.saga.saga.builder
+package io.simplesource.saga.saga.builder;
+
+import io.simplesource.kafka.dsl.KafkaConfig;
+import io.simplesource.kafka.spec.WindowSpec;
+import io.simplesource.saga.model.api.SagaAPI;
+import io.simplesource.saga.model.serdes.SagaSerdes;
+import io.simplesource.saga.model.specs.SagaSpec;
+import io.simplesource.saga.saga.api.KafkaSagaAPI;
+import io.simplesource.saga.shared.topics.TopicConfig;
+import io.simplesource.saga.shared.topics.TopicConfigBuilder;
+import io.simplesource.saga.shared.topics.TopicTypes;
+
+import java.util.Collections;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.Objects.requireNonNull;
+
+final class SagaClientBuilder<A> {
+
+    private final KafkaConfig kafkaConfig;
+    private final ScheduledExecutorService scheduler;
+    private SagaSerdes<A> serdes = null;
+    private TopicConfig topicConfig = null;
+    private String clientId = null;
+    private WindowSpec windowSpec = new WindowSpec(3600L);
+
+
+    public SagaClientBuilder(KafkaConfig kafkaConfig, ScheduledExecutorService scheduler) {
+        this.kafkaConfig = kafkaConfig;
+        this.scheduler = scheduler;
+    }
+
+    SagaClientBuilder<A> withSerdes(SagaSerdes<A> serdes) {
+        this.serdes = serdes;
+        return this;
+    }
+
+    SagaClientBuilder<A> withResponseWindow(WindowSpec windowSpec) {
+        this.windowSpec = windowSpec;
+        return this;
+    }
+
+    SagaClientBuilder<A> withTopicConfig(TopicConfigBuilder.BuildSteps topicBuildFn) {
+        TopicConfigBuilder tcb = new TopicConfigBuilder(TopicTypes.SagaTopic.client, Collections.emptyMap(), Collections.emptyMap());
+        topicBuildFn.applyStep(tcb);
+        this.topicConfig = tcb.build();
+        return this;
+    }
+
+    SagaClientBuilder<A> withClientId(String clientId) {
+        this.clientId = clientId;
+        return this;
+    }
+
+    SagaAPI<A> build() {
+        requireNonNull(serdes, "Serdes have not been defined");
+        requireNonNull(topicConfig, "TopicConfig has not been defined");
+        requireNonNull(clientId, "ClientId has not been defined");
+        SagaSpec<A> sagaSpec =new SagaSpec<>(serdes, windowSpec);
+        return new KafkaSagaAPI<>(sagaSpec, kafkaConfig, topicConfig, clientId, scheduler);
+    }
+}
 //
-//import java.util.concurrent.ScheduledExecutorService
+//object SagaClientBuilder {
+//  type BuildSteps<A> = SagaClientBuilder<A> => SagaClientBuilder<A>
+
+
 //
-//import io.simplesource.kafka.dsl.KafkaConfig
-//import io.simplesource.kafka.spec.WindowSpec
-//import model.api.SagaAPI
-//import model.serdes.SagaSerdes
-//import model.specs.SagaSpec
-//import saga.api.KafkaSagaAPI
-//import shared.topics.{TopicConfig, TopicConfigBuilder, TopicTypes}
+//final class SagaClientBuilder<A>(kafkaConfig: KafkaConfig, scheduler: ScheduledExecutorService) {
 //
-//final class SagaClientBuilder[A](kafkaConfig: KafkaConfig, scheduler: ScheduledExecutorService) {
-//
-//  var serdes: Option[SagaSerdes[A]]    = None
-//  var topicConfig: Option[TopicConfig] = None
-//  var clientId: Option[String]         = None
+//  var serdes: Option<SagaSerdes<A>>    = None
+//  var topicConfig: Option<TopicConfig> = None
+//  var clientId: Option<String>         = None
 //  var windowSpec: WindowSpec           = new WindowSpec(3600L)
 //
-//  def withSerdes(serdes: SagaSerdes[A]): SagaClientBuilder[A] = {
+//  def withSerdes(serdes: SagaSerdes<A>): SagaClientBuilder<A> = {
 //    this.serdes = Some(serdes)
 //    this
 //  }
 //
-//  def withResponseWindow(windowSpec: WindowSpec): SagaClientBuilder[A] = {
+//  def withResponseWindow(windowSpec: WindowSpec): SagaClientBuilder<A> = {
 //    this.windowSpec = windowSpec
 //    this
 //  }
 //
-//  def withTopicConfig(topicBuildFn: TopicConfigBuilder.BuildSteps): SagaClientBuilder[A] = {
+//  def withTopicConfig(topicBuildFn: TopicConfigBuilder.BuildSteps): SagaClientBuilder<A> = {
 //    val tcb = TopicConfigBuilder(TopicTypes.SagaTopic.client, Map.empty)
 //    topicBuildFn(tcb)
 //    val topicConfig = tcb.build()
@@ -35,20 +91,20 @@
 //    this
 //  }
 //
-//  def withClientId(clientId: String): SagaClientBuilder[A] = {
+//  def withClientId(clientId: String): SagaClientBuilder<A> = {
 //    this.clientId = Some(clientId)
 //    this
 //  }
 //
-//  def build(): SagaAPI[A] = {
+//  def build(): SagaAPI<A> = {
 //    assert(serdes.isDefined, "Serdes have not been defined")
 //    assert(topicConfig.isDefined, "TopicConfig has not been defined")
 //    assert(clientId.isDefined, "ClientId has not been defined")
 //    val sagaSpec = SagaSpec(serdes.get, windowSpec)
-//    new KafkaSagaAPI[A](sagaSpec, kafkaConfig, topicConfig.get, clientId.get, scheduler)
+//    new KafkaSagaAPI<A>(sagaSpec, kafkaConfig, topicConfig.get, clientId.get, scheduler)
 //  }
 //}
 //
 //object SagaClientBuilder {
-//  type BuildSteps[A] = SagaClientBuilder[A] => SagaClientBuilder[A]
+//  type BuildSteps<A> = SagaClientBuilder<A> => SagaClientBuilder<A>
 //}
