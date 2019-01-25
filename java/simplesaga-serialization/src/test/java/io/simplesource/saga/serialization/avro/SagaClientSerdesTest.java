e9fbe309a4a1e0a17be868b6ab8dc5324ae4bdf9
szoio <stephenzoio@yahoo.com>
2019-01-25 15:09:23 +1100

    added action serisalisers, started with saga serialisers

---
+++ java/simplesaga-serialization/src/test/java/io/simplesource/saga/serialization/avro/SagaClientSerdesTest.java
@@ -0,0 +1,57 @@
+package io.simplesource.saga.serialization.avro;
+
+import io.simplesource.data.Result;
+import io.simplesource.data.Sequence;
+import io.simplesource.saga.model.messages.SagaResponse;
+import io.simplesource.saga.model.saga.SagaError;
+import io.simplesource.saga.model.serdes.SagaClientSerdes;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SagaClientSerdesTest {
+
+    private static String SCHEMA_URL = "http://localhost:8081/";
+    private static String FAKE_TOPIC = "topic";
+
+    @Test
+    void uuidTest() {
+        SagaClientSerdes<?> serdes = AvroSerdes.sagaClientSerdes(SCHEMA_URL, true);
+        UUID original = UUID.randomUUID();
+        byte[] serialized = serdes.uuid().serializer().serialize(FAKE_TOPIC, original);
+        UUID deserialized = serdes.uuid().deserializer().deserialize(FAKE_TOPIC, serialized);
+        assertThat(deserialized).isEqualTo(original);
+    }
+
+    @Test
+    void responseTestSuccess() {
+        SagaClientSerdes<?> serdes = AvroSerdes.sagaClientSerdes(SCHEMA_URL, true);
+        SagaResponse original = new SagaResponse(UUID.randomUUID(), Result.success(Sequence.first().next().next()));
+        byte[] serialized = serdes.response().serializer().serialize(FAKE_TOPIC, original);
+        SagaResponse deserialized = serdes.response().deserializer().deserialize(FAKE_TOPIC, serialized);
+        assertThat(deserialized.toString()).isEqualTo(original.toString());
+        assertThat(deserialized).isEqualToIgnoringGivenFields(original, "result");
+        assertThat(deserialized.result.isSuccess()).isTrue();
+    }
+
+    @Test
+    void responseTestFailure() {
+        SagaClientSerdes<?> serdes = AvroSerdes.sagaClientSerdes(SCHEMA_URL, true);
+        SagaError sagaError1 = SagaError.of(SagaError.Reason.InternalError, "There was an error");
+        SagaError sagaError2 = SagaError.of(SagaError.Reason.CommandError, "Invalid command");
+        SagaResponse original = new SagaResponse(UUID.randomUUID(), Result.failure(sagaError1, sagaError2));
+        byte[] serialized = serdes.response().serializer().serialize(FAKE_TOPIC, original);
+        SagaResponse deserialized = serdes.response().deserializer().deserialize(FAKE_TOPIC, serialized);
+        assertThat(deserialized).isEqualToIgnoringGivenFields(original, "result");
+        assertThat(deserialized.result.isFailure()).isTrue();
+        deserialized.result.failureReasons().ifPresent(nel -> {
+            List<SagaError> el = nel.toList();
+            assertThat(el).hasSize(2);
+            assertThat(el.get(0)).isEqualToComparingFieldByField(sagaError1);
+            assertThat(el.get(1)).isEqualToComparingFieldByField(sagaError2);
+        });
+    }
+}
